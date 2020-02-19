efc8889b3e728cfa7a28cd8db664c7095abcfa81
coodyz <1848600800@qq.com>
2020-02-19 23:02:02 +0800

    1.修复若干常规BUG以及两个fatal BUG 2.暂不使用邮箱以及验证码功能

--- accoj/static/js/base.js
+++ accoj/static/js/base.js
@@ -4,7 +4,7 @@
  */
 
 $(function () {
-    $(".nav").find("li").each(function () {
+    $(".navbar-nav").find("li").each(function () {
         var a = $(this).find("a:first")[0];
         if ($(a).attr("href") === location.pathname) {
             $(this).addClass("active");
