65010f49dc1af5c5eb7190c832594b8413276791
coodyz <1848600800@qq.com>
2020-02-23 12:36:58 +0800

    1.增加确认提交模态框 2.用户信息提示框接口完善，增加可选参数--提示消息头部

--- accoj/static/js/base.js
+++ accoj/static/js/base.js
@@ -80,10 +80,22 @@ function check_email(email) {
  * @param message
  * @param message_type danger or info
  * @param timeout ms
+ * @param message_head
  */
-function show_message(id, message, message_type, timeout) {
+function show_message(id, message, message_type, timeout, message_head = false) {
     if (document.getElementById("show_message")) return;
-    $('#' + id).append("<div class='alert alert-" + message_type + "' id='show_message' style='text-align: center;display: none;'> <strong>" + message + "</strong></div>");
+    let div_content_base = "<div class='alert alert-" + message_type + "' id='show_message' style='text-align: center;display: none;'> <strong>";
+    let type = "提示!";
+    if (message_type === "danger") {
+        type = "错误!";
+    } else if (message_type === "warning") {
+        type = "警告!";
+    }
+    if (message_head) {
+        type = message_head;
+    }
+    let div_content = div_content_base + type + "</strong>" + message + "</div>";
+    $('#' + id).append(div_content);
     let show_message_tmp = $('#show_message');
     // 淡入效果
     show_message_tmp.fadeIn(timeout);
@@ -95,4 +107,49 @@ function show_message(id, message, message_type, timeout) {
     setTimeout(function () {
         show_message_tmp.remove()
     }, timeout * 2.5);
+}
+
+function show_submit_confirm(submit_deal_fun) {
+    submit_confirm_append(submit_deal_fun);
+    $('#submit_confirm').modal('show');
+}
+
+function close_submit_confirm() {
+    $('#submit_confirm').modal('hide');
+}
+
+function remove_submit_confirm() {
+    $('#submit_confirm').remove();
+}
+
+function submit_confirm_append(submit_deal_fun) {
+    let div_content = "<!-- 确认提交模态框（Modal） -->" +
+        '        <div class="modal fade" id="submit_confirm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">' +
+        '            <div class="modal-dialog">' +
+        '                <div class="modal-content">' +
+        '                     <div class="modal-header">' +
+        '                        <button type="button" class="close" data-dismiss="modal"' +
+        '                                aria-hidden="true">×' +
+        '                        </button>' +
+        '                        <h4 class="modal-title" id="myModalLabel" style="text-align: center">' +
+        '                            确认提交' +
+        '                        </h4>' +
+        '                    </div>' +
+        '                    <div id="submit_confirm_message_origin" class="alert alert-info" style="text-align: center">' +
+        '		                <strong>提示！</strong>确认提交后将不可更改。' +
+        '	                 </div>' +
+        '                    <div id="submit_confirm_message">' +
+        '                    </div>' +
+        '                    <div class="modal-footer">' +
+        '                        <button type="button" class="btn btn-default"' +
+        '                                data-dismiss="modal">关闭' +
+        '                        </button>' +
+        '                        <button type="button" class="btn btn-primary" id="submit_confirm_button" onclick="' + submit_deal_fun + '">' +
+        '                            确认提交' +
+        '                        </button>' +
+        '                    </div>' +
+        '                </div><!-- /.modal-content -->' +
+        '            </div><!-- /.modal-dialog -->' +
+        '        </div><!-- /.modal -->';
+    $("#base_header").append(div_content);
 }
