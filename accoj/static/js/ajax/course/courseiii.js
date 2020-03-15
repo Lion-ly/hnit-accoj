53d007704f9a4bb9a5b6427d9db543c74b3a75e5
coodyz <1848600800@qq.com>
2020-03-15 17:42:04 +0800

    1.常规BUG修复

--- accoj/static/js/ajax/course/courseiii.js
+++ accoj/static/js/ajax/course/courseiii.js
@@ -78,8 +78,8 @@ function map_subject_info(data) {
     subject_saved = data ? data["subject_saved"] : subject_saved;
 
     let business_index = now_business_no - 1,
-        confirmed = key_element_confirmed ? key_element_confirmed.indexOf(business_index) !== -1 : false,
-        saved = key_element_saved ? key_element_saved.indexOf(business_index) !== -1 : false;
+        confirmed = subject_confirmed ? subject_confirmed.indexOf(business_index) !== -1 : false,
+        saved = subject_saved ? subject_saved.indexOf(business_index) !== -1 : false;
 
     // 先清空box
     clear_box();
