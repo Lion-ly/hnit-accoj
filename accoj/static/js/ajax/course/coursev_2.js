04ee14b4b1f9ab705b66bf8d9350254bf2a097f3
coodyz <1848600800@qq.com>
2020-04-08 13:49:22 +0800

    平衡表前端“错误标记”实现。

--- accoj/static/js/ajax/course/coursev_2.js
+++ accoj/static/js/ajax/course/coursev_2.js
@@ -138,34 +138,76 @@ function v2GetInput() {
 function v2PaddingData(data, isFromButton) {
     function padding() {
         let accounting_period_1 = data["accounting_period_1"],
-            accounting_period_2 = data["accounting_period_2"];
+            accounting_period_2 = data["accounting_period_2"],
+            accounting_period_1_cp = "",
+            accounting_period_2_cp = "",
+            error_pos = Array();
+        if (isFromButton === 1) {
+            accounting_period_1_cp = answer_infos["accounting_period_2"];
+            accounting_period_2_cp = answer_infos["accounting_period_2"];
+        }
         // 创建行
         for (let i = 0; i < accounting_period_1.length - 2; i++) v2_AddRow(true);
         for (let i = 0; i < accounting_period_2.length - 2; i++) v2_AddRow(false);
 
         // 填充数据
-        let accounting_period = accounting_period_1,
+        let info = accounting_period_1,
+            info_cp = accounting_period_1_cp,
             index = 0;
         $("[id^=period1_row], [id=period1_last], [id^=period2_row], [id=period2_last]").each(function () {
-            let $this = $(this);
+            let $this = $(this),
+                t_info_cp = ""; // 答案的行
             if ($this.attr("id") === "period2_row_1") {
-                accounting_period = accounting_period_2;
+                info = accounting_period_2;
+                info_cp = accounting_period_2_cp;
                 index = 0;
             }
             let $thisInputs = $this.find("input"),
+                $thisInputs0 = $($thisInputs[0]),
                 keys = ["borrow_1", "lend_1", "borrow_2", "lend_2", "borrow_3", "lend_3"],
-                subject = accounting_period[index]["subject"],
+                subject = info[index]["subject"],
                 inputIndex = 0;
+            if (subject !== "sum") $thisInputs0.val(subject);
 
-            if (subject !== "sum") $($thisInputs[0]).val(subject);
+            if (isFromButton === 1) {
+                for (let i = 0; i < info_cp.length; i++) {
+                    if (subject === info_cp[i]["subject"]) {
+                        t_info_cp = info_cp[i];
+                        break;
+                    }
+                }
+                if (!t_info_cp) error_pos.push($thisInputs0);
+            }
 
             $thisInputs.each(function (t_index, item) {
+                let $item = $(item);
                 if (subject === "sum" && t_index === 0) {
-                    $(item).val(accounting_period[index][keys[inputIndex++]]);
-                } else if (t_index !== 0) $(item).val(accounting_period[index][keys[inputIndex++]]);
+                    let value = info[index][keys[inputIndex]],
+                        value_cp = "";
+                    $item.val(value);
+                    if (t_info_cp) {
+                        value_cp = t_info_cp[inputIndex];
+                        if (value_cp != value) error_pos.push($item);
+                    }
+                    inputIndex++;
+                } else if (t_index !== 0) {
+                    let value = info[index][keys[inputIndex]],
+                        value_cp = "";
+                    $item.val(value);
+                    if (t_info_cp) {
+                        value_cp = t_info_cp[inputIndex];
+                        if (value_cp != value) error_pos.push($item);
+                    } else if (isFromButton === 1 && !t_info_cp) error_pos.push($item);
+                    inputIndex++;
+                }
             });
             index++;
         });
+        if (isFromButton === 1) {
+            for (let i = 0; i < error_pos.length; i++)
+                hasError(error_pos[i]);
+        }
+        if (isFromButton) v2DisabledInput();
     }
 
     if (!data) return;
@@ -188,6 +230,7 @@ function v2Bind() {
         spanStatusCtr(true, true, "submit_status_span");
         v2PaddingData(answer_infos, 2);
     }
+
     bind_confirm_info("submit_balance_sheet_info");
     bind_save_info(submit_balance_sheet_info);
     bindAnswerSource("", map_balance_sheet_info, map_answer);
@@ -209,6 +252,19 @@ function v2Reset() {
     period2_row = 2;
     $("[id^=period1_row][id!=period1_row_1], [id^=period2_row][id!=period2_row_1]").remove();
     $("input").val("");
+}
+
+/**
+ * 禁用编辑
+ */
+function v2DisabledInput() {
+    let $inputs = $("div[class=courseBody]").find("input"),
+        $aLabels = $("a[type=button][data-toggle]"),
+        $button = $("button[data-save], button[data-confirm]");
+    $button.prop("disabled", true);
+    $inputs.attr("readonly", "readonly");
+    $aLabels.attr({"disabled": true, "onclick": ""});
+    $aLabels.unbind("click");
 }
 
 /*
