7b908a0d680d99d5a9ddd3704d8689fe19f4b58e
Chief <1732302584@qq.com>
2020-06-12 22:02:33 +0800

    # 修改了对None的处理

--- accoj/blueprints/api.py
+++ accoj/blueprints/api.py
@@ -6,6 +6,7 @@
 # @File    : api.py
 # @Software: PyCharm
 from flask import Blueprint, jsonify, request, session, send_from_directory, current_app
+from accoj.blueprints import get_schedule
 from accoj.utils import parse_class_xlrd, login_required_teacher
 from accoj.extensions import mongo
 from accoj.emails import assign_email
@@ -48,84 +49,8 @@ def profile_api():
         return jsonify(result=result, data=data, message=message)
 
     def get_user_schedule():
-        schedule_info = mongo.db.company.find_one(dict(student_no=student_no),
-                                                  dict(_id=0, schedule_confirm=1))
-        # 判断是否有进度
-        if schedule_info:
-            data = []
-            schedule_info = schedule_info.get("schedule_confirm")
-            # 获取涉及的科目数量
-            involve_subjects = mongo.db.company.find_one(dict(student_no=student_no),
-                                                         dict(_id=0, involve_subjects=1)).get("involve_subjects")
-            involve_subjects_1 = involve_subjects.get("involve_subjects_1")
-            involve_subjects_2 = involve_subjects.get("involve_subjects_2")
-            sum_subjects_len = len(set(involve_subjects_1 + involve_subjects_2))
-
-            # 获取进度值
-            trend_analysis_confirm = schedule_info.get("trend_analysis_confirm")
-            common_ratio_analysis_confirm = schedule_info.get("common_ratio_analysis_confirm")
-
-            if schedule_info.get("business_confirm"):
-                data.append(100)
-
-            key_element_score = int((len(schedule_info.get("key_element_confirm")) / 20) * 100)
-            data.append(key_element_score)
-
-            subject_score = int((len(schedule_info.get("subject_confirm")) / 20) * 100)
-            data.append(subject_score)
-
-            entry_schedule_score = int((len(schedule_info.get("entry_confirm")) / 20) * 100)
-            data.append(entry_schedule_score)
-
-            # ledger_confirm   25 25 50
-            ledger__schedule_score = 0
-            ledger__schedule_score += int(
-                (len(schedule_info.get("ledger_confirm").get("ledger1_confirm")) / len(involve_subjects_1)) * 25)
-            ledger__schedule_score += int(
-                (len(schedule_info.get("ledger_confirm").get("ledger2_confirm")) / len(involve_subjects_2)) * 25)
-            if schedule_info.get("balance_sheet_confirm"):
-                ledger__schedule_score += 50
-            data.append(ledger__schedule_score)
-
-            acc_document_schedule_score = int((len(schedule_info.get("acc_document_confirm")) / 20) * 100)
-            data.append(acc_document_schedule_score)
-
-            # 会计账簿部分得分
-            account_schedule_score = 0
-            account_schedule_score += int(
-                (len(schedule_info.get("subsidiary_account_confirm")) / sum_subjects_len) * 50)
-            if schedule_info.get("acc_balance_sheet_confirm"):
-                account_schedule_score += 50
-            data.append(account_schedule_score)
-
-            # 会计报表部分得分
-            Financial_Statements_schecule_score = 0
-            if schedule_info.get("new_balance_sheet_confirm"):
-                Financial_Statements_schecule_score += 50
-            if schedule_info.get("profit_statement_confirm"):
-                Financial_Statements_schecule_score += 50
-            data.append(Financial_Statements_schecule_score)
-
-            # 因素分析未做  20*5 or 15*4+20*2
-            analysis_schedule_score = 0
-            if trend_analysis_confirm.get("first"):
-                analysis_schedule_score += 20
-            if trend_analysis_confirm.get("second"):
-                analysis_schedule_score += 20
-            if common_ratio_analysis_confirm.get("first"):
-                analysis_schedule_score += 20
-            if common_ratio_analysis_confirm.get("second"):
-                analysis_schedule_score += 20
-            if schedule_info.get("ratio_analysis_confirm"):
-                analysis_schedule_score += 20
-            data.append(analysis_schedule_score)
-
-            # 杜邦
-            dupont_schedule_score = 0
-            if schedule_info.get("dupont_analysis_confirm"):
-                dupont_schedule_score += 100
-            data.append(dupont_schedule_score)
-
+        data = get_schedule(student_no)
+        if data:
             result, data = True, data
         else:
             data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -133,18 +58,18 @@ def profile_api():
         return jsonify(result=result, data=data)
 
     def get_user_score():
-        user = mongo.db.user.find_one(dict(student_no=student_no),
-                                      dict(_id=1))
-        if user:
-            a_keys = ['_id', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
-            a_values = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-            search_dict = dict(zip(a_keys, a_values))
-            data_tmp = mongo.db.score.find_one({"student_no": "17020340139"}, search_dict)
+        a_keys = ['_id', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
+        a_values = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        search_dict = dict(zip(a_keys, a_values))
+        data_tmp = mongo.db.score.find_one({"student_no": student_no}, search_dict)
+        if data_tmp:
             data = list(data_tmp.values())
             result = True
+            print("score")
         else:
-            result = False
-            data = None
+            result = True
+            data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            print("no score")
         return jsonify(result=result, data=data)
 
     json_data = request.get_json()
